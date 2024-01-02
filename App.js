4086288112ab7305ab27e44a43678b14eb7401ae
Samanvith <Samanvith2005@gmail.com>
2024-01-02 16:53:48 +0530

    Added App.js and index.css

---
+++ App.js
@@ -0,0 +1,5 @@
+
+      
+      const header = React.createElement("h1", {id:"header"}, "Hello world By React");
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(header);
