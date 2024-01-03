0dada2f6b14e8f883b9396a05d640e621ea93d4a
Samanvith <Samanvith2005@gmail.com>
2024-01-03 13:05:37 +0530

    Installed react,reactDom and added BrowserList to the package.json

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const header = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
     "div",
-    { id: "parent" }, 
-    React.createElement(
-      "div",
-      { id: "child" }, 
-      React.createElement("h1", {}, "Hello Good morning"),
-      React.createElement("h2", {}, "Hello Good evening")
-    )
-  );
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(header);
-  
+    { id: "child" },
+    React.createElement("h1", {}, "Namaste React"),
+    React.createElement("h2", {}, "Hello Good evening")
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
