f546030a103b2b840515ef78ed98ec0cd3078183
Samanvith <Samanvith2005@gmail.com>
2024-01-04 12:36:07 +0530

    Added Functional Component

--- App.js
+++ App.js
@@ -1,5 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-const jsxheading=(<h1 id="header" className="footer">Hello Good morning</h1>)
+import ReactDOM from "react-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxheading);
+root.render(<FooterComponent />);
+
+
+
+
+
+
+
+// Functional Component for Heading
+// const HeadingComponent = function Heading() {
+//   return <h1>This is Heading</h1>;
+// };
+
+//  Functional Component for Footer
+// const FooterComponent = function Footer() {
+//   return (
+//     <div>
+//       <HeadingComponent/>
+//       <h2>This is Footer</h2>
+//     </div>
+//   );
+// };
+
+//1.jsx element-->ReactcreateElement-->ReactElement-->jsobject-->HTMlElement(render)
+// const jsxheading=(<h1 id="header" className="footer">Hello Good morning</h1>)
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxheading);
