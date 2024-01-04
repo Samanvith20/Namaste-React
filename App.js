f56d351b727c88a8a208114eb9caf1f258486ed6
Samanvith <Samanvith2005@gmail.com>
2024-01-04 11:30:07 +0530

    Added jsx into code

--- App.js
+++ App.js
@@ -1,16 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-const header = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "Namaste React"),
-    React.createElement("h2", {}, "Hello Good evening")
-  )
-);
-
+import ReactDOM from "react-dom/client";
+const jsxheading=(<h1 id="header" className="footer">Hello Good morning</h1>)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(header);
+root.render(jsxheading);
