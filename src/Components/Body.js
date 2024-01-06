c4aef176be5781d9a00be16df0c4b49f8e307a30
Samanvith <Samanvith2005@gmail.com>
2024-01-06 11:20:58 +0530

    Added useState

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,19 +1,38 @@
 import Restaurantcards from "./RestaurantCard";
-import resList from "../utils/dummydata";
-const Body = () => {
-    return (
-      <div className="Body">
-        <div className="search">
-          Search
-        </div>
-        <div className="res-container">
-          
-        {resList.map((restaurant) => (
-  <Restaurantcards key={restaurant.id} resdata={restaurant} />
-))}
+import Dummydata from "../utils/dummydata";
+import { useState } from "react";
 
-        </div>
+const Body = () => {
+  // methods for writing a use state
+   const[restaurantsData ,setRestaurantsData]=useState(Dummydata)
+
+   
+  //  const array = useState(Dummydata)--> Destructing the array
+  //  2.const [restaurantsData ,setRestaurantsData]=array
+  // 3. const restaurantsData= array[0]
+  //   const setRestaurantsData =array[1]
+
+
+  const handleFilterClick = () => {
+    const filteredRestaurants = restaurantsData.filter((res) => res.rating > 4);
+    
+    setRestaurantsData(filteredRestaurants);
+  };
+
+  return (
+    <div className="Body">
+      <div className="search">
+        <button className="flt-btn" onClick={handleFilterClick}>
+          Filter button
+        </button>
       </div>
-    );
-  }
-  export default Body
+      <div className="res-container">
+        {restaurantsData.map((restaurant) => (
+          <Restaurantcards key={restaurant.id} resdata={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -1,21 +1,24 @@
- import { IMG_URL } from "../utils/constants";
- const Restaurantcards = (props) => {
-    const { resdata } = props;
-  
-    if (!resdata || !resdata.restaurants) {
-      return <div>No data available</div>;
-    }
-  
-    const { name, cuisines, rating, deliveryTime } = resdata?.restaurants
-  
-    return (
-      <div className="card-logo">
-        <img alt="card-image" src={IMG_URL} />
-        <h3>{name}</h3>
-        <h4>{cuisines}</h4>
-        <h3>{rating}</h3>
-        <p>{deliveryTime}</p>
-      </div>
-    );
-  };
-   export default Restaurantcards
+import React from "react";
+import { IMG_URL } from "../utils/constants";
+
+const Restaurantcards = (props) => {
+  const { resdata } = props;
+
+  if (!resdata) {
+    return <div>No data available</div>;
+  }
+
+  const { name, cuisines, rating, deliveryTime } = resdata;
+
+  return (
+    <div className="card-logo">
+      <img alt="card-image" src={IMG_URL} />
+      <h3>{name}</h3>
+      <h4>{cuisines}</h4>
+      <h3>{rating}</h3>
+      <p>{deliveryTime}</p>
+    </div>
+  );
+};
+
+export default Restaurantcards;
